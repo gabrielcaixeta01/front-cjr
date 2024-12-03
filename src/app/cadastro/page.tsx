f62dfa90b683ec94037681d11ee252585c1cc1a5
Henrique Sales <hcrsaless@gmail.com>
2024-12-03 15:05:30 -0300

    add formik to cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,9 +1,39 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Insira o seu nome"),
+  email: Yup.string()
+    .email("Insira um email válido")
+    .required("O e-mail é obrigatório"),
+  password: Yup.string()
+    .min(6, "A senha deve ter pelo menos 6 caracteres")
+    .required("A senha é obrigatória"),
+  department: Yup.string().required("O departamento é obrigatório"),
+  course: Yup.string().required("O curso é obrigatório"),
+});
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  department: "",
+  course: "",
+};
 
 export default function Cadastro() {
   const router = useRouter();
+  const onSubmit = (values: typeof initialValues) => {
+    const filteredValues = Object.fromEntries(
+      Object.entries(values).filter(([, value]) => value !== "")
+    );
+    console.log("Dados salvos:", filteredValues);
+    router.push("/feed/Logado");
+  };
+  //const [showPassword, setShowPassword] = useState(false);
   return (
     <div className="w-full h-screen flex  relative">
       <div className="ImgContainer h-full flex-1 relative">
@@ -27,49 +57,84 @@ export default function Cadastro() {
           height={150}
           className="mt-4 rounded-full"
         />
-        <input
-          id="nome"
-          type="text"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-16 "
-          placeholder="Nome"
-        />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {() => (
+            <Form className="w-3/4 flex flex-col items-center">
+              <Field
+                name="name"
+                type="text"
+                className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-16 "
+                placeholder="Nome"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-red-500 text-sm mt-2"
+              />
 
-        <input
-          id="email"
-          type="text"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
-          placeholder="Email"
-        />
+              <Field
+                name="email"
+                type="text"
+                className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
+                placeholder="Email"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500 text-sm mt-2"
+              />
 
-        <input
-          id="senha"
-          type="password"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
-          placeholder="Senha"
-        />
+              <Field
+                name="password"
+                type="password"
+                className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
+                placeholder="Senha"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-red-500 text-sm mt-2"
+              />
 
-        <input
-          id="curso"
-          type="text"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
-          placeholder="Curso"
-        />
+              <Field
+                name="course"
+                type="text"
+                className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
+                placeholder="Curso"
+              />
+              <ErrorMessage
+                name="course"
+                component="div"
+                className="text-red-500 text-sm mt-2"
+              />
 
-        <input
-          id="departamento"
-          type="text"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
-          placeholder="Departamento"
-        />
+              <Field
+                name="department"
+                type="text"
+                className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8"
+                placeholder="Departamento"
+              />
+              <ErrorMessage
+                name="department"
+                component="div"
+                className="text-red-500 text-sm mt-2"
+              />
 
-        <section className="mt-16">
-          <button
-            className="bg-green-300 border-[0.125rem] border-gray-500 p-2 rounded-lg hover:scale-110 duration-200 w-40 h-12 text-xl"
-            onClick={() => router.push("/feed/Logado")}
-          >
-            Criar Conta
-          </button>
-        </section>
+              <section className="mt-16">
+                <button
+                  type="submit"
+                  className="bg-green-300 border-[0.125rem] border-gray-500 p-2 rounded-lg hover:scale-110 duration-200 w-40 h-12 text-xl"
+                >
+                  Criar Conta
+                </button>
+              </section>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
