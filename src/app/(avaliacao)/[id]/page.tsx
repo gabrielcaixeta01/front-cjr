2293668a86a392b4ccb891c6dd1188a46283a6a7
Pedro Neris <pedrolucaspn13@gmail.com>
2024-12-10 17:33:17 -0300

    feat: começando integração do modal de avaliação

--- src/app/(avaliacao)/[id]/page.tsx
+++ src/app/(avaliacao)/[id]/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { BellIcon } from "@heroicons/react/24/solid";
-
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/20/solid";
+import getAval from "@/app/utils/api";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/compat/router";
 
 export default function Avaliacao() {
   const router = useRouter();
   return (
     <>
       <div className="flex flex-col h-screen bg-gray-100">
-          <div className="flex justify-between w-full bg-customGreen p-2 items-center fixed">
+          <div className="flex justify-between w-full bg-blue-200 p-2 items-center fixed">
             <div className= "pl-3">
             <Image
                 src="/logounb.png"
@@ -23,10 +25,10 @@ export default function Avaliacao() {
             <div className="flex justify-between pr-3 items-center cursor-pointer"> 
               <div className="pr-12">
                 <button
-                className="bg-white hover:bg-blue-600 mx-5 p-2 rounded-[60px] transition duration-300 ease-in-out"
+                  className="bg-azulCjr hover:bg-blue-600 mx-5 p-2 rounded-[60px] transition duration-300 ease-in-out"
                 onClick={() => alert("Sem notificações novas.")}
                 >
-                <BellIcon className="h-6 w-6 text-black" /> 
+                <BellIcon className="h-6 w-6 text-white" /> 
                 </button>
               </div>
 
@@ -43,24 +45,17 @@ export default function Avaliacao() {
 
               <div> 
                 <button
-                className="bg-white hover:bg-blue-600 mx-5 p-2 rounded-[60px] transition duration-300 ease-in-out"
-                onClick={() => router.push("/login")}
-                >
-                  <Image
-                  src="/sair.png"
-                  alt= "Ícone de sair"
-                  width={40}
-                  height={40}
-                  className="h-6 w-6 text-white"
+                  className="flex items-center bg-azulCjr text-white rounded-[60px] px-4 py-2 hover:bg-blue-600 transition duration-300 ease-in-out mx-5"
+                  onClick={() => router.push("/feed/Deslogado")}
                   >
-                  </Image>
+                  <ArrowRightOnRectangleIcon className="h-6 w-6 text-white" />
               </button>
               </div>
             </div>
           </div>
         <div className="w-full h-screen max-w-[40%] mt-10 mx-auto bg-white rounded shadow-md ">
           <div className=" w-full max-w-[95%] bg-[#3EEE9A] rounded-md mt-8 flex flex-col mx-auto">
-              <div className=" w-full max-w-[100%] flex flex-col mx-auto border-b-2 border-b-black pb-[0.7rem]">
+              <div className=" w-full max-w-[100%] flex flex-col mx-auto border-b-[1.5px] border-b-black pb-[0.7rem]">
                 <div className="flex mx-auto items-center p-3 pb-1 ">
                     <div className="pl-1 items-center">
                     <Image
@@ -68,11 +63,11 @@ export default function Avaliacao() {
                       alt="Foto de perfil"
                       width={48}
                       height={48}
-                      className="w-9 h-9 rounded-full shadow-md bg-white object-cover"
+                      className="w-9 h-9 rounded-full shadow-md bg-white object-cover cursor-pointer"
                       onClick={() => router.push("/perfil/Aluno/Logado")}
                     />
                     </div>
-                    <span className="font-sans text-black pl-2 text-[15px] font-[500] leading-[16.94px] items-center"> Morty Gamer </span>
+                    <span className="font-sans text-black ml-2 text-[15px] font-[500] leading-[16.94px] items-center hover:bg-blue-200 transition duration-300 ease-in-out cursor-pointer" onClick={()=> router.push("/perfil/Aluno/Logado")}> Morty Gamer </span>
                     <span className="font-sans text-[#71767B] text-[12px] font-[350] leading-[16.94px] flex pl-1.5 items-center"> · 08/04/2024, ás 21:42 </span>
                     <span className="font-sans text-[#71767B] text-[12px] font-[350] leading-[16.94px] flex pl-1 items-center"> · Homer Simpson </span>
                     <span className="font-sans text-[#71767B] text-[12px] font-[350] leading-[16.94px] flex pl-1 items-center"> · Engenharia Química </span>
@@ -91,28 +86,30 @@ export default function Avaliacao() {
                       />
                       <span className="font-sans text-[#222E50] text-[12px] font-[600] leading-[14.52px] flex pl-1 items-center"> 2 comentários</span>
                   </div>
-                                 
                   <div className="flex pr-2">
+                    {/*<button className=" hover:bg-blue-600 rounded-[60px] transition duration-300 ease-in-out"> 
+                      <TrashIcon className="text-black h-4 w-4"/> 
+                      </button>*/}              
                     <Image
                       src="/editar.png"
                       alt="Icone de editar"
                       width={64} 
                       height={64}
-                      className="w-4 h-4 object-cover mx-2"
+                      className="w-4 h-4 object-cover mx-2 shadow-md hover:bg-blue-200 transition duration-300 ease-in-out cursor-pointer"
                     />
                     <Image
                       src="/lixeira.png"
                       alt="Icone de editar"
                       width={64} 
                       height={64}
-                      className="w-4 h-4 object-cover mx-2"
+                      className="w-4 h-4 object-cover mx-2 shadow-md hover:bg-blue-200 transition duration-300 ease-in-out cursor-pointer"
                     />   
                   </div>
 
                 </div>
                 
               </div>
-              <div className=" pt-[1rem] w-full max-w-[70%] flex flex-col mx-auto border-b-2 border-b-gray-300 mb-2 justify-center">
+              <div className=" pt-[1rem] w-full max-w-[70%] flex flex-col mx-auto border-b-[1.6px] border-b-gray-300 mb-2 justify-center">
                 <div className="flex mx-left pb-[0.2rem] items-center">         
                   <div className="items-center">
                       <Image
@@ -120,34 +117,34 @@ export default function Avaliacao() {
                         alt="Foto de perfil"
                         width={48}
                         height={48}
-                        className="w-7 h-7 rounded-full shadow-md bg-white object-cover"
+                        className="w-7 h-7 rounded-full shadow-md bg-white object-cover cursor-pointer"
                         onClick={() => router.push("/perfil/Aluno/Logado")}
                       />
                   </div>
-                  <span className="font-sans text-black pl-2 text-[13px] font-[500] leading-[15.73px] text-center items-center"> El Gabi </span> 
+                  <span className="font-sans text-black ml-2 text-[13px] font-[500] leading-[15.73px] text-center items-center hover:bg-blue-200 transition duration-300 ease-in-out cursor-pointer"> El Gabi </span> 
                   <span className="font-sans text-[#71767B] pl-2 text-[13px] font-[350] leading-[15.73px] text-center items-center"> · 08/04/2024, ás 21:43  </span>                                   
                 </div>
                 <div className="pl-[2.3rem]"> 
                   <p className="text-[#222E50] text-[14px] text-[500] leading-[16.94px] pb-2"> Vou pro Cruzeiro! </p>
                 </div>
               </div>
-              <div className="pt-[0.3rem] w-full max-w-[70%] flex flex-col mx-auto border-b-2 border-b-gray-300 mb-2 justify-center">
-                <div className="flex mx-left pb-[0.2rem] items-center">         
+              <div className="pt-[0.3rem] w-full max-w-[70%] flex flex-col mx-auto mb-2 justify-center">
+                <div className="flex mx-left mb-[0.2rem] items-center">         
                   <div className="items-center">
                       <Image
                         src="/felipeluis.jpeg"
                         alt="Foto de perfil"
                         width={48}
                         height={48}
-                        className="w-7 h-7 rounded-full shadow-md bg-white object-cover"
+                        className="w-7 h-7 rounded-full shadow-md bg-white object-cover cursor-pointer"
                         onClick={() => router.push("/perfil/Aluno/Logado")}
                       />
                   </div>
-                  <span className="font-sans text-black pl-2 text-[13px] font-[500] leading-[15.73px] text-center items-center"> Guardiola Cabeludo </span> 
-                  <span className="font-sans text-[#71767B] pl-2 text-[13px] font-[350] leading-[15.73px] text-center items-center"> · 08/04/2024, ás 21:43  </span>                                   
+                  <span className="font-sans text-black ml-2 text-[13px] font-[500] leading-[15.73px] text-center items-center hover:bg-blue-200 transition duration-300 ease-in-out cursor-pointer"> Guardiola Cabeludo </span> 
+                  <span className="font-sans text-[#71767B] ml-2 text-[13px] font-[350] leading-[15.73px] text-center items-center"> · 08/04/2024, ás 21:43  </span>                                   
                 </div>
                 <div className="pl-[2.3rem]"> 
-                  <p className="text-[#222E50] text-[14px] text-[500] leading-[16.94px] pb-2"> Entreguei pro criciuma nassaum  </p>
+                  <p className="text-[#222E50] text-[14px] text-[500] leading-[16.94px] mb-2 "> Entreguei pro criciuma nassaum  </p>
                 </div>
               </div>
 
--- src/app/modals/avaliacao/page.tsx
+++ src/app/modals/avaliacao/page.tsx
@@ -1,30 +1,65 @@
 "use client";
-import {useState} from "react"
+import {useEffect, useState} from "react"
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { getAllProfs } from "@/app/utils/api";
+import { useParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { profile } from "console";
+
 export default function Avaliacao() {
   const [mostrarProfs, setMostrarProfs] = useState(false);
+  const [mostrarDisciplinas, setMostrarDisciplinas] = useState(false);
+  const [texto, setTexto] = useState("");
+  const [textoAval, setTextoAval]= useState("");
+  const [listaProfs, setListaProfs] = useState <any[]>([])
+  const router = useRouter()
+  const [idProfAvaliacao, setIdProfAvaliacao] = useState("-1")
+
+  const getProfs = async () => {
+    try {
+      const professores = await getAllProfs();
+      setListaProfs (professores)
+    } catch (error){
+      console.log(error)
+    }
+  }
+  useEffect (()=>{
+    getProfs()
+  },[])
+
+
   return ( 
   <>
     <div className="flex flex-col h-screen bg-gray-100 justify-center">
         <div className="h-screen  w-1/2 max-h-[58%]  flex flex-col mx-auto bg-[#3EEE9A] rounded-md items-center">
-            <div className="flex bg-white h-[2rem] w-[90%] justify-between items-center hover:cursor-pointer rounded-md mt-6" onClick={()=>setMostrarProfs(!mostrarProfs)}> 
-                <span className="text-[#999797] font-[300] text-[18px] leading-[29.05px] pl-2" /*onClick={()=>{lista.map(profs)}=>}*/> Nome do professor </span> 
-            </div>
+        <select className="flex flex-col bg-white h-[2rem] w-[90%] justify-between items-left hover:cursor-pointer rounded-md text-[#999797] font-[300] text-[18px]  mt-6 leading-[3rem]" onChange={event}> 
+        <option value="-1" disabled selected className="text-[#999797] font-[300] text-[18px] leading-[29.05px] pl-2">
+      Nome do professor
+      </option>
+            {listaProfs.map((prof) => (
+              <option key={prof.id} value={prof.id} className="text-black">
+                {prof.name}
+              </option>))}
+            
+  
+
+        </select>
+            
+
             <div className="flex bg-white h-[2rem] w-[90%] justify-between items-center hover:cursor-pointer rounded-md mt-4"> 
-                <span className="text-[#999797] font-[300] text-[18px] leading-[29.05px] pl-2"> Disciplina </span> 
+                <span className="text-[#999797] font-[300] text-[18px] leading-[29.05px] pl-2" onClick={()=> setMostrarDisciplinas(!mostrarDisciplinas)}> Disciplina </span> 
             </div>
             <div className="flex flex-col h-[12rem] w-[90%] bg-[#A4FED3] mt-[1.5rem] rounded-md">
-              <div className="border-b-[1.5px] border-b-[#2B895C] pt-[0.5px] pl-4 pb-1"> 
-                <p> temporario </p>
-              </div>
-              <input type="text" className=" text-black h-16 w-full pt-[0.5px] pl-[1rem] rounded-md bg-[#A4FED3] leading-tight focus:outline-none"/> 
+              <input type="text" value={texto} onChange={(event)=> setTexto(event.target.value)} className=" text-black h-16 w-full pt-[0.5px] pl-[1rem] rounded-md bg-[#A4FED3] leading-tight focus:outline-none"/> 
             </div>
             <div className="ml-auto items-right pr-5 mt-6">
-              <button
+              <button onClick={()=> setTexto("")}
                 className="bg-transparent rounded-lg hover:scale-110 duration-200 w-20 h-10 text-xl text-[23px] font-400 leading-[54.46px] mr-9"
                 >
                 Cancelar
             </button>
-            <button
+            <button   onClick={() => texto == ""? toast.error("A avaliação não pode ser vazia") : (setTextoAval(texto), setTexto(""), toast.success("A avaliação foi criada com sucesso", {autoClose:2200}))}
                 className="bg-[#A4FED3] text-[#2B895C] font-400 text-[20px] rounded-lg hover:scale-110 duration-200 w-32 h-10 text-xl leading-[42.36px]  mr-10 ml-2"
                 >
                 Avaliar
