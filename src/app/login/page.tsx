5eb4ba838be1f4fca315d414ae9c93ae1d1b164d
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-12-16 00:54:17 -0300

    atualiza login com formik

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,43 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { getUserByEmail } from "@/utils/api";
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Insira o seu nome"),
+  email: Yup.string()
+    .email("Insira um email válido")
+    .required("O e-mail é obrigatório")
+    .test(
+      "checkEmailExists",
+      "Não existe conta cadastrada com esse email, se cadastre primeiro.",
+      async (email) => {
+        if (!email) return false;
+        try {
+          const existingUser = await getUserByEmail(email);
+          return existingUser;
+        } catch (error) {
+          console.error("Erro ao verificar email:", error);
+          return true;
+        }
+      }
+    ),
+  password: Yup.string().required("A senha é obrigatória"),
+});
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 export default function Login() {
   const router = useRouter();
+
+  const onSubmit = async () => {
+    router.push("/feed/Logado");
+  };
   return (
     <div className="w-full h-screen flex  relative">
       <div className="ImgContainer h-full flex-1 relative">
@@ -19,35 +53,54 @@ export default function Login() {
           <h1>Professores</h1>
         </div>
 
-        <input
-          id="email"
-          type="text"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-20 text-black"
-          placeholder="Email"
-        />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          <Form className="w-full flex flex-col items-center">
+            <Field
+              name="email"
+              type="email"
+              className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8 text-black"
+              placeholder="Email"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-red-500 mt-1 text-sm"
+            />
 
-        <input
-          id="senha"
-          type="password"
-          className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8 text-black"
-          placeholder="Senha"
-        />
+            <Field
+              name="password"
+              type="password"
+              className="w-3/4 h-12 p-2 border-[0.125rem] border-gray-300 rounded-lg focus:border-gray-500 mt-8 text-black"
+              placeholder="Senha"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="text-red-500 mt-1 text-sm"
+            />
 
-        <section className="mt-20 flex space-x-6">
-          <button
-            className="bg-green-300 border-[0.125rem] border-gray-500 p-2 rounded-lg hover:scale-110 duration-200 w-40 h-12 text-xl text-black"
-            onClick={() => router.push("/feed/Logado")}
-          >
-            Entrar
-          </button>
+            <section className="mt-20 flex space-x-6">
+              <button
+                type="submit"
+                className="bg-green-300 border-[0.125rem] border-gray-500 p-2 rounded-lg hover:scale-110 duration-200 w-40 h-12 text-xl text-black"
+              >
+                Entrar
+              </button>
 
-          <button
-            className="bg-green-300 border-[0.125rem] border-gray-500 p-2 rounded-lg hover:scale-110 duration-200 w-40 h-12 text-xl text-black"
-            onClick={() => router.push("/cadastro")}
-          >
-            Criar Conta
-          </button>
-        </section>
+              <button
+                type="button"
+                onClick={() => router.push("/cadastro")}
+                className="bg-green-300 border-[0.125rem] border-gray-500 p-2 rounded-lg hover:scale-110 duration-200 w-40 h-12 text-xl text-black"
+              >
+                Criar Conta
+              </button>
+            </section>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
