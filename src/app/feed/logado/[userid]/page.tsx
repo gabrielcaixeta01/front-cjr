6bff886a0edfb2a491624c9bdc52ac99a1bd3f47
Pedro Neris <pedrolucaspn13@gmail.com>
2024-12-17 10:12:06 -0300

    fix: readicionando botao e modal de nova avaliação no feed logado

--- src/app/feed/logado/[userid]/page.tsx
+++ src/app/feed/logado/[userid]/page.tsx
@@ -5,8 +5,8 @@ import { useParams, useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import axios from "axios"; // Importação corrigida
 import { toast } from "react-toastify";
-import { fetchUserInfo } from "@/utils/api";
-import { User, Professor } from "@/types";
+import { fetchUserInfo, createAval } from "@/utils/api";
+import { User, Professor, Avaliacao } from "@/types";
 import { BellIcon, ArrowRightOnRectangleIcon } from "@heroicons/react/24/solid";
 
 
@@ -17,6 +17,16 @@ export default function FeedLogado() {
   const [userInfo, setUserInfo] = useState<User | null>(null);
   const [professores, setProfessores] = useState<Professor[]>([]);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [texto, setTexto] = useState("");
+  const [listaProfs, setListaProfs] = useState<Professor[]>([]);
+  const [listaCourses, setListaCourses] = useState<Course[]>([]);
+  const [profSelected, setProfSelected] = useState("-1");
+  const [courseSelected, setCourseSelected] = useState("-1");
+  const [idProfAvaliacao, setIdProfAvaliacao] = useState("-1");
+  const [idCourseAvaliacao, setIdCourseAvaliacao] = useState("-1");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
 
   // Carrega as informações do usuário
   useEffect(() => {
@@ -83,6 +93,94 @@ export default function FeedLogado() {
     setFilteredProfessores(sortedProfessores);
     setIsPopupOpen(false);
   };
+
+  //função para criar avaliação no banco de dados
+  const creatingAval = async (aval: Partial<Avaliacao>) => {
+    try {
+      await createAval(aval); // Remova a variável 'created' se não for necessária
+      console.log("Avaliação criada com sucesso.");
+    } catch (error) {
+      console.error("Erro ao criar avaliação:", error);
+    }
+  };
+
+  //reseta campos do modal de avaliação quando é fechado
+  const resetModalFields = () => {
+    setTexto("");
+    setProfSelected("-1");
+    setCourseSelected("-1");
+    setIdProfAvaliacao("-1");
+    setIdCourseAvaliacao("-1");
+  };
+
+  const toggleModal= () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  //modal de avaliação usado na página
+  const modalAvaliacao = () => {
+    const modal = 
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50 ">
+      <div className="h-screen  w-[60%] max-h-[62%]  flex flex-col mx-auto bg-[#3EEE9A] rounded-md items-center">
+        <select value= {profSelected} className="flex flex-col bg-white h-[2rem] w-[90%] justify-between items-left hover:cursor-pointer rounded-md text-[#999797] font-[300] text-[18px] mt-6 leading-[3rem] pt-1" onChange={(event)=> {setIdProfAvaliacao(event.target.value); setProfSelected(event.target.value)}}>
+          <option value="-1"  disabled className="text-[#999797] font-[300] text-[18px] leading-[29.05px] pl-2">
+            Nome do professor
+          </option>
+          {listaProfs.map((prof) => (
+            <option key={prof.id} value={prof.id} className="text-black text-[18px] leading-[29.05px] font-[200] ">
+              {prof.name}
+            </option>))}
+        </select>
+    
+        <select value={courseSelected} className="flex flex-col bg-white h-[2rem] w-[90%] justify-between items-left hover:cursor-pointer rounded-md text-[#999797] font-[300] text-[18px]  mt-6 leading-[3rem] pt-1" onChange={(event)=> {setIdCourseAvaliacao(event.target.value); setCourseSelected(event.target.value)}}>
+          <option value="-1"  disabled  className="text-[#999797] font-[300] text-[18px] leading-[29.05px] pl-2">
+            Disciplina
+          </option>
+          {listaCourses.map((course) => (
+            <option key={course.id} value={course.id} className="text-black text-[18px] leading-[29.05px] font-[200]">
+              {course.name}
+            </option>))}           
+        </select>
+
+        <div className="flex flex-col h-[12rem] w-[90%] bg-[#A4FED3] mt-[1.5rem] rounded-md">
+          <textarea value={texto} placeholder= "Digite sua avaliação" onChange={(event)=> setTexto(event.target.value)} className=" text-black placeholder-black placeholder-opacity-50 mt-2 mr-[0.3px]h-full w-full pt-[0.5px] pl-[1rem] rounded-md bg-[#A4FED3] leading-tight focus:outline-none resize-none overflow-y-auto" > </textarea>
+        </div>
+
+        <div className="ml-auto items-right pr-5 mt-6">
+          <button onClick={() => { resetModalFields(); toggleModal(); }}
+            className="bg-transparent rounded-lg hover:scale-110 duration-200 w-20 h-10 text-xl text-[23px] font-400 leading-[54.46px] mr-9"
+            >
+            Cancelar
+          </button>
+          <button className="bg-[#A4FED3] text-[#2B895C] font-400 text-[20px] rounded-lg hover:scale-110 duration-200 w-32 h-10 text-xl leading-[42.36px]  mr-10 ml-2"
+             onClick={() => {
+            if (texto === "") {
+              toast.error("A avaliação não pode ser vazia", {autoClose:2200});
+            } else if (parseInt(idProfAvaliacao, 10) === -1) {
+              toast.error("Selecione um professor", {autoClose:2200});
+            } else if (parseInt(idCourseAvaliacao, 10) === -1) {
+              toast.error("Selecione uma disciplina", {autoClose:2200});
+            } else {
+              const newAval: Partial<Avaliacao> = {
+                text: texto,
+                professorId: parseInt(idProfAvaliacao, 10),
+                courseId: parseInt(idCourseAvaliacao, 10),
+                userId: Number(userid),
+              };
+              creatingAval(newAval);
+              resetModalFields();
+              toast.success("A avaliação foi criada com sucesso", { autoClose: 2200 });
+              toggleModal();
+            }
+          }}>
+            Avaliar
+          </button>
+        </div>
+      </div>
+    </div>     
+  return modal;       
+  }
+
 
   return (
     <div className="bg-gray-100 min-h-screen">
@@ -175,6 +273,17 @@ export default function FeedLogado() {
                 </div>
               </div>
             )}
+
+          <div className="flex  mx-auto w-[20%] max-w-[30%] max-h-[2%]">
+            <button
+              className="bg-azulCjr text-white w-[11rem] h-[3.5rem] mx-auto rounded-lg shadow-md hover:shadow-lg hover:bg-blue-600 transition duration-500"
+              onClick={toggleModal}>
+              Criar nova avaliação
+            </button>
+            {isModalOpen && (
+              modalAvaliacao()
+            )}                              
+          </div>
       
             {/* Grid de Professores */}
             <section className="p-4 grid w-[70%] min-h-fit mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
